81663b27ccaecf3a52116eb6271042ef0f700736
Emerson <emerzon.lc@gmail.com>
2023-08-03 17:34:00 -0500

    fix: merge conflicts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent } from "../common/types";
 import bookList from "../data/books.json";
-import React, { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import LayoutTypeInput from "../components/ui/LayoutTypeInput.tsx";
 import { GridLayoutIcon, ListLayoutIcon } from "../assets/icons";
@@ -52,13 +52,22 @@ const getMinMaxPages = (
 type LayoutType = "grid" | "list";
 
 const Home = (): FunctionComponent => {
-  const [books] = useState(bookList.library);
+  const [books, setBooks] = useState(bookList.library);
 
   const genres = getGenres(books);
   const { minPage, maxPage } = getMinMaxPages(books);
   const [numberPage, setNumberPage] = useState(maxPage);
   const [selectedGenre, setSelectedGenre] = useState("Todas");
   const [layout, setLayout] = useState<LayoutType>("grid"); // grid | list
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
 
   const handlePageNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNumberPage(Number(event.target.value));
@@ -83,8 +92,48 @@ const Home = (): FunctionComponent => {
 
   const isGridLayout = layout === "grid";
 
+  // const handleDelete = (ISBN: string) => {
+  //   const deleteBooks = books.filter((books) => books.book.ISBN !== ISBN);
+  //   setBooks(deleteBooks);
+  // };
+  const Dialog = ({
+    isOpen,
+    onClose,
+  }: {
+    isOpen: boolean;
+    onClose: () => void;
+  }) => {
+    if (!isOpen) return null;
+
+    return (
+      <div className="absolute bg-[#232222cf]  w-full h-full">
+        <div className="absolute left-1/2 top-1/4 -translate-x-1/2 z-10  bg-[#F2F2F2]  p-10 flex flex-col items-center gap-10 w-60">
+          <p>Are you sure to delete this book?</p>
+          <div className="flex gap-4">
+            <button
+              className="bg-[#b3aeaee7] text-[#ffffff] border-double border-4 rounded-md w-24 py-2"
+              onClick={onClose}
+            >
+              Close
+            </button>
+            <button
+              onClick={() => {
+                // handleDelete(books.book.ISBN);
+              }}
+              className="bg-[#1C73E8] text-[#ffffff] border-double border-4 rounded-md w-24 py-2"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="container mx-auto px-4 py-10">
+    <div className="relative container mx-auto px-4 py-10">
+      <Dialog isOpen={isDialogOpen} onClose={handleCloseDialog} />
+
       <h1 className="text-4xl mb-10 font-bold">
         Lista de libros{" "}
         {filteredBooks.length > 0 && (
@@ -168,11 +217,32 @@ const Home = (): FunctionComponent => {
                   : "flex justify-between border-b-2 pb-7 py-10 items-center"
               }
             >
-              <img
-                className={isGridLayout ? "aspect-[2/3]" : "h-32  object-cover"}
-                src={book.book.cover}
-                alt=""
-              />
+              <div className="group relative">
+                <div className="absolute w-full h-full hover:bg-[#1e1e1e6e] ">
+                  <div></div>
+                  <button
+                    onClick={handleOpenDialog}
+                    // onClick={() => {
+                    //   handleDelete(book.book.ISBN);
+                    // }}
+                    className="hidden group-hover:flex absolute right-2 top-2 w-8 h-8"
+                  >
+                    <img
+                      className="absolute w-full h-full"
+                      src="/icons/trash-icon.svg"
+                      alt=""
+                    />
+                  </button>
+                </div>
+                <img
+                  className={
+                    isGridLayout ? "aspect-[2/3]" : "h-32  object-cover"
+                  }
+                  src={book.book.cover}
+                  alt=""
+                />
+              </div>
+
               <h2 className="text-xl font-bold capitalize">
                 {book.book.title}
               </h2>
