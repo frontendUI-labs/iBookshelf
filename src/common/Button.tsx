a122361c3af65ef3f3ec8105e784f73c3771c0c5
paulclindo <paulclindo@gmail.com>
2023-08-04 17:53:55 -0500

    refactor: button component

---
+++ src/common/Button.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+type ButtonProps = {
+  onClick?: () => void;
+  children: ReactNode;
+  variant?: "primary" | "danger" | "secondary";
+};
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  variant = "primary",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={twMerge(
+        "inline-flex items-center rounded-md px-4 text-base font-medium h-9",
+        "ring-white ring-opacity-60 ring-offset-2 ring-offset-neutral-800 focus:outline-none focus:ring-2",
+        "hover:scale-105 duration-100 focus:scale-110",
+        variant === "primary" && "text-[rgb(250_250_250)] bg-[rgb(24_24_27)]",
+        variant === "secondary" &&
+          "text-[rgb(24_24_27)] bg-[rgba(228_228_231)]",
+        variant === "danger" && "text-[rgb(250_250_250)] bg-[rgb(239_68_68)]"
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
