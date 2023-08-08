69509ba2504ea085e6fce1ecdba5621049a1d42b
Wagner Lindo <128859360+WagnerLindo@users.noreply.github.com>
2023-08-08 15:59:03 -0700

    feat:createBook (#7)
    * feat:createBook
    
    * fix:library
    
    * fix:dropdowGnres
    
    * fix: add new book
    
    ---------
    
    Co-authored-by: paulclindo <paulclindo@gmail.com>

---
+++ src/components/ui/AddInput.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AddInputorText = ({
+  label,
+  id,
+  value,
+  onChange,
+  isTextArea = false,
+}: {
+  label: string;
+  id: string;
+  value: string | number | undefined;
+  onChange: (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => void;
+  isTextArea?: boolean;
+}) => {
+  return (
+    <div className="w-full flex flex-col gap-2 p-2">
+      <label htmlFor={id}>{label}</label>
+      {isTextArea ? (
+        <textarea
+          className="border-2 p-3 rounded-md hover:border-indigo-600"
+          name="textarea"
+          id={id}
+          value={value}
+          rows={5}
+          cols={8}
+          onChange={onChange}
+        ></textarea>
+      ) : (
+        <input
+          onChange={onChange}
+          className="border-2 p-3 rounded-md hover:border-indigo-600"
+          type="text"
+          id={id}
+          value={value}
+        />
+      )}
+    </div>
+  );
+};
+export default AddInputorText;
--- src/components/ui/GenreSelect.tsx
+++ src/components/ui/GenreSelect.tsx
@@ -5,17 +5,19 @@ export default function GenreSelect({
   value,
   onChange,
   options,
+  label,
 }: {
   options: string[];
   value: string;
   onChange: (value: string) => void;
+  label: string;
 }) {
   return (
     <Select.Root value={value} onValueChange={onChange}>
       <div className="flex flex-col gap-2">
-        <p>Filtrar por genero</p>
+        <p>{label}</p>
         <Select.Trigger
-          className="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
+          className="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[50px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
           aria-label="Genre"
         >
           <Select.Value placeholder="Select genre" />
@@ -28,14 +30,14 @@ export default function GenreSelect({
         <Select.Content
           sideOffset={10}
           position="popper"
-          className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
+          className="overflow-hidden bg-white rounded-md w-full shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
         >
           <Select.Viewport className="p-[5px]">
             {options.map((option) => (
               <Select.Item
                 key={option}
                 value={option}
-                className="text-sm leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                className="text-sm leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 hover:text-blue-500"
               >
                 <Select.ItemText>{option}</Select.ItemText>
                 <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
