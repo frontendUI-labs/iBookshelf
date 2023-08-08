3a73541134289e8da4d79f78fe1f9a7c7212eedb
Emerson <emerzon.lc@gmail.com>
2023-08-08 18:14:35 -0500

    fix:logs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,8 +10,8 @@ import GenreSelect from "../components/ui/GenreSelect.tsx";
 import Button from "../common/Button.tsx";
 import { Trash2, Pencil, PlusSquare } from "lucide-react";
 import EditInput from "../components/ui/EditInput.tsx";
-import * as Dialog from "@radix-ui/react-dialog";
 import AddInputorText from "../components/ui/AddInput.tsx";
+import * as Dialog from "@radix-ui/react-dialog";
 
 type Author = {
   name: string;
