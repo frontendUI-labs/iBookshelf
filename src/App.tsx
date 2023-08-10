ead171447d9465fe577040e4a632ea54d391fba4
Brayan Ccari Lindo <brayancclindo@gmail.com>
2023-08-10 12:13:09 -0500

    fix:router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { RouterProvider, Link } from "@tanstack/router";
+import { RouterProvider } from "@tanstack/router";
 import type { FunctionComponent } from "./common/types";
 // import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
 import { router } from "./routes";
 
 const queryClient = new QueryClient();
 
-function Navigation() {
-  return (
-    // <div>holaaa</div>
-    <nav>
-      <ul>
-        {/* <li>
-          <Link to="/">Home</Link>
-        </li> */}
-        <li>
-          <Link to="/">HomePage</Link>
-        </li>
-        <li>
-          <Link to="/filter">filterPage</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
 const App = (): FunctionComponent => {
   return (
     <>
-      {/* <Navigation /> */}
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         {/* <TanStackRouterDevelopmentTools
