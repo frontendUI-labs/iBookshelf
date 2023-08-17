0002265843d0cd0bb29f8623c14b8fcff8a0e5d7
paulclindo <paulclindo@gmail.com>
2023-08-16 19:06:08 -0500

    fix: remove imports

--- src/pages/FilterPage.tsx
+++ src/pages/FilterPage.tsx
@@ -3,7 +3,6 @@ import Header from "../components/layout/header";
 import SideBar from "../components/ui/SideBar";
 import MainContent from "../components/ui/MainContent";
 import OnSaleBook from "../components/ui/on-sale-book";
-import useGetBooksWithPagination from "../hooks/use-get-pagination-with-book.ts";
 
 function Filter() {
   return (
