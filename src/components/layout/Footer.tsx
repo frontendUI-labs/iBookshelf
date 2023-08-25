31dd3668d4f684973170b6b01b6b6cf7b3fd10ae
Emerson <emerzon.lc@gmail.com>
2023-08-25 13:16:41 -0500

    fix:responsiveModeTwo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -17,7 +17,7 @@ import { twMerge } from "tailwind-merge";
 
 const ContactCard = () => {
   return (
-    <div className="container m-auto relative overflow-hidden text-white font-heading bg-purple-600 rounded-[32px] flex flex-col justify-center items-center gap-12 py-16">
+    <div className="container m-auto relative overflow-hidden text-white font-heading bg-purple-600 rounded-[32px] py-10  lg:py-16">
       <img
         className="absolute top-0 left-0"
         src="/images/triangleGroup.png"
@@ -33,18 +33,20 @@ const ContactCard = () => {
 
       <Triangle className="fill-[#6153BB] text-[#6153BB] absolute bottom-48 -right-64 w-[365px] h-[395px]" />
       <Triangle className="fill-[#FFAB7C] text-[#FFAB7C] absolute bottom-48 -right-72 w-[365px] h-[395px]" />
-      <h2 className="text-center text-[40px] font-medium ">
-        Subscribe our newsletter for newest <br /> books updates
-      </h2>
-      <div className="flex">
-        <input
-          className="w-[450px] rounded-l-xl border-2 border-transparent  py-4 px-6 text-base font-normal bg-[#7C6DE3] text-gray-200 focus:text-white"
-          type="text"
-          placeholder="Type your email here"
-        />
-        <button className="bg-white text-lg font-semibold text-black py-4 px-8 rounded-r-xl hover:scale-105 duration-300">
-          SUBSCRIBE
-        </button>
+      <div className="w-full relative flex flex-col gap-8 justify-center items-center lg:gap-12">
+        <h2 className="text-2xl text-center  font-medium lg:text-5xl">
+          Subscribe our newsletter for newest <br /> books updates
+        </h2>
+        <div className="flex flex-col items-center gap-4 lg:flex-row lg:gap-0">
+          <input
+            className="w-[300px]  border-2 border-transparent  py-4 px-6 text-base font-normal bg-[#7C6DE3] text-gray-200 focus:text-white lg:w-[450px] lg:rounded-l-xl"
+            type="text"
+            placeholder="Type your email here"
+          />
+          <button className="w-[150px] bg-white text-lg font-semibold text-black py-4 px-8 hover:scale-105 duration-300 lg:rounded-r-xl">
+            SUBSCRIBE
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -58,10 +60,10 @@ const SocialsIcons = ({
   children: ReactNode;
 }) => {
   return (
-    <a href="">
+    <a href="" target="_blank">
       <div
         className={twMerge(
-          "relative w-[58px] h-[58px] rounded-xl border-2 border-gray-300 flex justify-center items-center hover:scale-110",
+          "relative w-[48px] h-[48px] rounded-xl border-2 border-gray-300 flex justify-center items-center lg:w-[58px] lg:h-[58px] hover:scale-110",
           hover
         )}
       >
@@ -83,16 +85,16 @@ const IconYt = () => {
 };
 const Information = ({ icon, info }: { icon: ReactNode; info: string }) => {
   return (
-    <div className="flex items-center gap-3 w-[340px]">
+    <div className="flex items-center gap-3 lg:w-[340px]">
       <div className="icon-container">{icon}</div>
-      <p className="text-lg font-semibold">{info}</p>
+      <p className="text-sm font-semibold lg:text-lg">{info}</p>
     </div>
   );
 };
 
 const LinkTo = ({ category }: { category: string }) => {
   return (
-    <a href="">
+    <a href="" target="_blank">
       <li className="hover:text-purple-600">{category}</li>
     </a>
   );
@@ -102,27 +104,31 @@ const Footer = () => {
   const iconClassName = "text-purple-600 w-[25px] h-[25px]";
   return (
     <>
-      <div className="mt-40 flex flex-col gap-20 mb-4 pb-10 border-b-2 border-gray-300 ">
+      <div className="mt-40 flex flex-col gap-20 mb-4 pb-10 border-b-2 border-gray-300">
         <ContactCard />
-        <div className="container m-auto flex justify-between mt-16 w-full font-heading">
-          <div className="flex flex-col gap-6 w-[420px]">
+        <div className="px-5 lg:container m-auto flex flex-col gap-6  font-heading lg:mt-16 lg:flex-row justify-between">
+          <div className="follow w-full flex flex-col gap-2 lg:w-[420px] lg:gap-6">
             <div className="flex items-center gap-[20px]">
               <div className="bg-purple-600 p-3 rounded-lg ">
-                <img src="/icons/Vector.svg" alt="" />
+                <img className="w-8 lg:w-full" src="/icons/Vector.svg" alt="" />
               </div>
-              <div className="flex flex-col">
-                <img src="/icons/Bookoe.svg" alt="" />
+              <div className="flex flex-col gap-2 lg:gap-0">
+                <img
+                  className="w-20 lg:w-full"
+                  src="/icons/Bookoe.svg"
+                  alt=""
+                />
                 <span className="text-[13px]">Book Store Website</span>
               </div>
             </div>
-            <p className="text-base font-basic font-normal mb-4">
-              iBookShelf is a Book Store Website lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud
+            <p className="text-sm font-basic font-normal lg:mb-4 lg:text-base">
+              "Discover a world of knowledge and imagination at our books store.
+              Find your next literary adventure with us."
             </p>
             <div>
-              <h4 className="text-xl font-semibold mb-7">Follow Us</h4>
+              <h4 className="text-base font-semibold mb-2 lg:mb-7 lg:text-xl">
+                Follow Us
+              </h4>
               <div className="flex gap-4">
                 <SocialsIcons
                   hover="hover:bg-[#232aed3c]"
@@ -150,9 +156,11 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div>
-            <h4 className="text-xl font-semibold mb-4">Books Categories</h4>
-            <ul className="list-none text-lg font-normal grid grid-cols-2 gap-y-2 gap-x-20">
+          <div className="categories">
+            <h4 className="text-base font-semibold mb-4 lg:text-xl">
+              Books Categories
+            </h4>
+            <ul className="list-none text-sm font-normal grid grid-cols-2 gap-y-2 gap-x-20 lg:text-lg">
               <LinkTo category="Action" />
               <LinkTo category="Law" />
               <LinkTo category="Advanture" />
@@ -164,7 +172,7 @@ const Footer = () => {
               <LinkTo category="Drama" />
               <LinkTo category="TV Series" />
               <LinkTo category="Fantasy" />
-              <a href="">
+              <a href="" target="_blank">
                 <div className="text-purple-600 flex items-end">
                   View more
                   <ChevronRight className="text-purple-600" />
@@ -173,9 +181,11 @@ const Footer = () => {
               <LinkTo category="Horror" />
             </ul>
           </div>
-          <div>
-            <h4 className="text-xl font-semibold mb-4">Quick Links</h4>
-            <ul className="text-lg font-normal flex flex-col gap-2">
+          <div className="quicklinks">
+            <h4 className="text-sm font-semibold mb-4 lg:text-xl">
+              Quick Links
+            </h4>
+            <ul className="grid grid-cols-2 text-sm font-normal lg:flex flex-col gap-2 lg:text-lg">
               <LinkTo category="About us" />
               <LinkTo category="Contact us" />
               <LinkTo category="Products" />
@@ -185,13 +195,13 @@ const Footer = () => {
               <LinkTo category="Shipment" />
             </ul>
           </div>
-          <div className="flex flex-col gap-5">
-            <h4 className="text-xl font-semibold ">Our Store</h4>
+          <div className="information flex flex-col gap-2 lg:gap-5">
+            <h4 className="text-sm font-semibold lg:text-xl">Our Store</h4>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.5063418319506!2d-75.23073076847245!3d-12.04177453244419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x910e961dd48c148f%3A0x47dd04a6231d4ff3!2sJr.%20Turmalina%2C%20Huancayo%2012006!5e0!3m2!1ses-419!2spe!4v1692639151151!5m2!1ses-419!2spe"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6862.37426942483!2d-122.44929124613022!3d37.78804581372756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808580cc9a17e597%3A0x7004d2aa23cd50ec!2sCalifornia%20St%2C%20San%20Francisco%2C%20CA%2094107%2C%20EE.%20UU.!5e0!3m2!1ses-419!2spe!4v1692986626821!5m2!1ses-419!2spe"
               width="600"
               height="450"
-              className="border-none h-[120px] w-[380px] rounded-xl hover:scale-105 duration-200"
+              className="border-none w-full h-[120px] lg:w-[380px] rounded-xl hover:scale-105 duration-200"
               // allowfullscreen=""
               loading="lazy"
               // referrerpolicy="no-referrer-when-downgrade"
@@ -211,14 +221,14 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="container m-auto pb-4 flex items-center justify-between font-heading text-base font-normal">
+      <div className="px-4 lg:container m-auto pb-4 flex flex-col lg:flex-row items-center justify-between font-heading text-xs font-normal lg:text-base">
         <p>iBookshrelf Book Store Website - © 2023 All Rights Reserved</p>
         <p className="flex gap-2 items-center">
           Made with
           <span>
             <Heart className="fill-red-500 text-red-500 w-5 h-5" />
           </span>
-          by Team Legendary
+          by FrontendUI Labs
         </p>
       </div>
     </>
